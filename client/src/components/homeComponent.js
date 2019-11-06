212e829718285369614959c469cb2d6330e75973
George <georuiz817@gmail.com>
2019-11-06 16:37:03 -0500

    starting delete function

--- client/src/components/homeComponent.js
+++ client/src/components/homeComponent.js
@@ -1,11 +1,12 @@
 import React from 'react'; 
 import '/Users/dawn/gobbler-project/client/src/Css/home.css'
+
 const homeComponent = () => {
     return(
     <div>
         <h1 id="gobblerHeader">Gobbler</h1>
         <hr className="borderh1"></hr>
-        <ol class="gobblerList">
+        <ol className="gobblerList">
             <li>Create your ideal Thanksgiving dish using our Gobbler form.</li>
             <li>Smile! Your Gobbler dish will be replicated by one of our volunteers
                 and served to a person in need this Thanksgiving day.
