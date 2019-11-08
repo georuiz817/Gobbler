6bafe792878bcc0ab4d466eeedc1f4fe0cba2e8e
George <georuiz817@gmail.com>
2019-11-07 22:49:04 -0500

    going crazy with css, footer nav, home, images all updated

--- client/src/components/homeComponent.js
+++ client/src/components/homeComponent.js
@@ -1,16 +1,15 @@
 import React from 'react'; 
 import '/Users/dawn/gobbler-project/client/src/Css/home.css'
+import homeLogo from "/Users/dawn/gobbler-project/client/src/photos/large-logo.png";
 
 const homeComponent = () => {
     return(
     <div>
-        <h1 id="gobblerHeader">Gobbler</h1>
+        <img id="gobblerHeader" src={homeLogo} alt="n/a"/>
         <hr className="borderh1"></hr>
         <ol className="gobblerList">
             <li>Create your ideal Thanksgiving dish using our Gobbler form.</li>
-            <li>Smile! Your Gobbler dish will be replicated by one of our volunteers
-                and served to a person in need this Thanksgiving day.
-            </li>
+            <li>Smile! Your Gobbler dish will be replicated by one of our volunteers and served to a person in need this Thanksgiving day.</li>
         </ol>
     </div>
          )
