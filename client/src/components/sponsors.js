2b9ce7c678b74f10b1789ab1bd3a19121f38fde7
George <georuiz817@gmail.com>
2019-11-11 15:01:01 -0500

    sponsor page done/fixed - fixing reducers

--- client/src/components/sponsors.js
+++ client/src/components/sponsors.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import '/Users/dawn/gobbler-project/client/src/Css/sponsors.css';
-import jet from '/Users/dawn/gobbler-project/client/src/photos/jet.jpg'; 
-import butter from '/Users/dawn/gobbler-project/client/src/photos/butter-ball.jpg';
-import ups from '/Users/dawn/gobbler-project/client/src/photos/ups-logo.jpeg';
-import army from '/Users/dawn/gobbler-project/client/src/photos/Army.png';
+import butter from '/Users/dawn/gobbler-project/client/src/photos/butter-ball.png';
+import ups from '/Users/dawn/gobbler-project/client/src/photos/upsLogo.jpeg';
+import army from '/Users/dawn/gobbler-project/client/src/photos/armyL.png';
 import judes from '/Users/dawn/gobbler-project/client/src/photos/st-judes.jpeg';
-import nintendo from '/Users/dawn/gobbler-project/client/src/photos/nintendo-logo.jpg'
+import nintendo from '/Users/dawn/gobbler-project/client/src/photos/nintendoL.png'
 
 
 const Sponsors = () => {
@@ -13,7 +12,6 @@ const Sponsors = () => {
   <div>
     <div class="row">
       <div class="column">
-        <img src={jet} alt="n/a"/>
         <img src={butter} alt="n/a"/>
         <img src={ups} alt="n/a"/>
         <img src={army} alt="n/a"/>
