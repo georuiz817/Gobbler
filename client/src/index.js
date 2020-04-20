9dcbb3fb57556dbc4b27490c01d02571853e2caf
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-04-20 13:57:51 -0400

    client update for heroku

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,6 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk' 
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import dishesReducer from './reducers/dishesReducer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -20,7 +19,3 @@ ReactDOM.render(
 );
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
