6a6f207d61b33f9d4d139bff33b81fff8a9f813e
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-06-13 22:40:34 -0400

    bootstrapped navbar properly

--- client/src/components/navComponent.js
+++ client/src/components/navComponent.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import Navbar from 'react-bootstrap/Navbar'
 
 const Nav = () => {
     return (
       <div className="nav">
-        <Link to="/"><p>Home</p></Link>
-        <Link to="/dishesNew"><p>Create Dish</p></Link>
-        <Link to="/dishesIndex"><p>View all Dishes</p></Link>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Link to="/"><p>Home</p></Link>
+            <Link to="/dishesNew"><p>Create Dish</p></Link>
+            <Link to="/dishesIndex"><p>View all Dishes</p></Link>
+          </Nav>
+        </Navbar>
       </div>
     )
   }
