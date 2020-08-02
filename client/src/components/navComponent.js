3c58549d616a537f61521f1168ba64965dedb3e2
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-08-02 16:30:36 -0400

    fixed navbar project working properly

--- client/src/components/navComponent.js
+++ client/src/components/navComponent.js
@@ -1,26 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar'
 
 const Nav = () => {
     return (
       <div className="nav">
-        <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Link to="/"><p>Home</p></Link>
-            <Link to="/dishesNew"><p>Create Dish</p></Link>
-            <Link to="/dishesIndex"><p>View all Dishes</p></Link>
-          </Nav>
-        </Navbar>
+        <Link to="/"><p>Home</p></Link>
+        <Link to="/dishesNew"><p>Create Dish</p></Link>
+        <Link to="/dishesIndex"><p>View all Dishes</p></Link>
       </div>
     )
   }
 
 export default Nav
-
-
-
-
 
 
